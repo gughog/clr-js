3d8d765d288f6fa613973140b72493d63612750f
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2019-11-18 11:12:37 -0300

    Some tweaks

--- clr.js
+++ clr.js
@@ -1,12 +1,12 @@
 // colors:
-let white   = "\x1b[37m"
-let black   = "\x1b[30m"
-let red     = "\x1b[31m"
-let green   = "\x1b[32m"
-let blue    = "\x1b[34m"
-let yellow  = "\x1b[33m"
-let magenta = "\x1b[35m"
-let cyan    = "\x1b[36m"
+let white   = ["\x1b[37m", "\x1b[0m"]
+let black   = ["\x1b[30m", "\x1b[0m"]
+let red     = ["\x1b[31m", "\x1b[0m"]
+let green   = ["\x1b[32m", "\x1b[0m"]
+let blue    = ["\x1b[34m", "\x1b[0m"]
+let yellow  = ["\x1b[33m", "\x1b[0m"]
+let magenta = ["\x1b[35m", "\x1b[0m"]
+let cyan    = ["\x1b[36m", "\x1b[0m"]
 
 // Specials
 let bolds     = ["\033[1m", "\033[0m"]
@@ -14,34 +14,34 @@ let underline = ["\033[4m", "\033[0m"]
 let darky     = ["\033[2m", "\033[0m"]
 let stroke    = ["\033[9m", "\033[0m"]
 // colors:
-let bwhite = "\x1b[47m"
-let bblack = "\x1b[40m"
-let bred = "\x1b[41m"
-let bgreen = "\x1b[42m"
-let bblue = "\x1b[44m"
-let byellow = "\x1b[43m"
-let bmagenta = "\x1b[45m"
-let bcyan = "\x1b[46m"
+let bwhite   = ["\x1b[47m", "\x1b[0m"]
+let bblack   = ["\x1b[40m", "\x1b[0m"]
+let bred     = ["\x1b[41m", "\x1b[0m"]
+let bgreen   = ["\x1b[42m", "\x1b[0m"]
+let bblue    = ["\x1b[44m", "\x1b[0m"]
+let byellow  = ["\x1b[43m", "\x1b[0m"]
+let bmagenta = ["\x1b[45m", "\x1b[0m"]
+let bcyan    = ["\x1b[46m", "\x1b[0m"]
 
 module.exports =  {
   // Colors
-  white:   (str) => `${white}${str}\x1b[0m`,
-  black:   (str) => `${black}${str}\x1b[0m`,
-  red:     (str) => `${red}${str}\x1b[0m`,
-  green:   (str) => `${green}${str}\x1b[0m`,
-  blue:    (str) => `${blue}${str}\x1b[0m`,
-  yellow:  (str) => `${yellow}${str}\x1b[0m`,
-  magenta: (str) => `${magenta}${str}\x1b[0m`,
-  cyan:    (str) => `${cyan}${str}\x1b[0m`,
+  white:   (str) => `${white[0]}${str}${white[1]}`,
+  black:   (str) => `${black[0]}${str}${black[1]}`,
+  red:     (str) => `${red[0]}${str}${red[1]}`,
+  green:   (str) => `${green[0]}${str}${green[1]}`,
+  blue:    (str) => `${blue[0]}${str}${blue[1]}`,
+  yellow:  (str) => `${yellow[0]}${str}${yellow[1]}`,
+  magenta: (str) => `${magenta[0]}${str}${magenta[1]}`,
+  cyan:    (str) => `${cyan[0]}${str}${cyan[1]}`,
   // Background colors
-  bwhite:   (str) => `${bwhite}${str}\x1b[0m`,
-  bblack:   (str) => `${bblack}${str}\x1b[0m`,
-  bred:     (str) => `${bred}${str}\x1b[0m`,
-  bgreen:   (str) => `${bgreen}${str}\x1b[0m`,
-  bblue:    (str) => `${bblue}${str}\x1b[0m`,
-  byellow:  (str) => `${byellow}${str}\x1b[0m`,
-  bmagenta: (str) => `${bmagenta}${str}\x1b[0m`,
-  bcyan:    (str) => `${bcyan}${str}\x1b[0m`,
+  bwhite:   (str) => `${bwhite[0]}${str}${bwhite[1]}`,
+  bblack:   (str) => `${bblack[0]}${str}${bblack[1]}`,
+  bred:     (str) => `${bred[0]}${str}${bred[1]}`,
+  bgreen:   (str) => `${bgreen[0]}${str}${bgreen[1]}`,
+  bblue:    (str) => `${bblue[0]}${str}${bblue[1]}`,
+  byellow:  (str) => `${byellow[0]}${str}${byellow[1]}`,
+  bmagenta: (str) => `${bmagenta[0]}${str}${bmagenta[1]}`,
+  bcyan:    (str) => `${bcyan[0]}${str}${bcyan[1]}`,
   // Specials
   bold :    (colorstr) => `${bolds[0]}${colorstr}${bolds[1]}`,
   uline:    (colorstr) => `${underline[0]}${colorstr}${underline[1]}`,
@@ -52,4 +52,5 @@ module.exports =  {
 /*
 TODO:
 - More colors;
+- Two Variant colors
 */
